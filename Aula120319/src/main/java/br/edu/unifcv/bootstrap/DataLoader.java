9f091373ed287fdd03f0c479536ebc6d578cc49e
André Luiz de Oliveira <andre.npd@hotmail.com>
2019-03-12 19:48:48 -0300

    Base do trabalho.

---
+++ Aula120319/src/main/java/br/edu/unifcv/bootstrap/DataLoader.java
@@ -0,0 +1,15 @@
+package br.edu.unifcv.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+
+	@Override
+	public void run(String... args) throws Exception {		
+		// insira os dados para testar
+	}
+
+}
